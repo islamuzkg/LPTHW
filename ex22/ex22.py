ad77f459183a7d89c4155cd5bb92a162c8709391
Islom Akhmedov <iakhmedov@centos.localdomain>
2017-10-23 09:41:26 -0500

    updated file

--- ex22/ex22.py
+++ ex22/ex22.py
@@ -1,4 +1,82 @@
-# ex13.py Parameterd, Unpacking, Variables (I would also call it argument)
+
+# ex01.py "A Good First Program"
+- print: Display text in counsole
+
+----------------------------------------------------
+# ex02.py "Comments And POund Characters"
+- #: Comment/diable a line
+
+-----------------------------------------------------
+# ex03.py
+- + plus
+- - minus
+- / slash
+- * asterisk
+- %  percent (but used as modulo/modules_dict)
+- < less-than
+- > greater-than
+- <+ less-than-equal
+- >= greater-than-equal
+Per https://www.youtube.com/watch?v=8nmekdC2r-c, the "modulus" is not the remainder; it's the divisor used as a basis for determining the remainder.... "Modulo" is the relational term modulus, 
+and we use it to briefly state a number is being taken with a certain modules: a mod b (read "a modulo b")
+
+-----------------------------------------------------
+# ex04.py "Variables and Names" 
+- _: underscore in variable names
+- floating point:  numbers with decimals
+- integerg: number w/o decimals
+
+----------------------------------------------------
+# ex05.py "More Variables and Printing"
+- format strings
+- %s: to put a value in a string
+- %: provide a value after the string
+- %d: for numbers ("digits")
+- %r: for "raw data" (atually, "representation")
+
+---------------------------------------------------
+# ex06.py "Strings and Text"
+- ': single quote
+- ": double quote
+
+--------------------------------------------------
+# ex07.py "More Printing"
+- +: for joining the strings w/o spaces
+- ,: for joining strings with spaces
+---------------------------------------------------
+# ex08.py "Printing, Printing"
+- True: Boolean value/keyword that does not need quotes
+- False: Boolean value/keyword that does not need quotes
+
+----------------------------------------------------
+# ex09.py "Printing, Printing, Printing"
+- \: backslash, used to escape characters
+- \n: new line command
+- """: triple quotes to set off several lines of text
+
+----------------------------------------------------
+# ex10.py "What Was That?"
+- \\: to escape a backslash \ within a string
+- \': to escape a sing quote within a string
+- \": to escape a doble quote within a string
+- \r: carriage return within a string
+- \t: tab within a string
+- ''': Alternative method of triple quotes
+
+
+------------------------------------------------------
+# ex11.py "Asking Questions" 
+- raw_input: to get typed input from user
+
+--------------------------------------------------------
+# ex12.py "Prompting People"
+- raw_input(*Prompt text: " Alternative way to put prompt in for raw_input from user
+- pydoc: Shell command for information on Python topics
+- pydoc topics: raw_input (ON Windows use python -m pydoc raw_input), open file, os, sys
+
+
+-----------------------------------------------------
+# ex13.py "Parameterd, Unpacking, Variables (I would also call it argument)"
 
 from sys import argv # import is used to call a  feature your script
 
@@ -16,7 +94,7 @@ print "Your first variabl is:" first #unpacking the argument
 - module: the name for the collections of extra code that can be imported into Python
 
 -------------------------------------------------------
-# ex14.py  Prompting and Passing 
+# ex14.py  "Prompting and Passing"
 from sys import argv
 
 script, user_name = argv
@@ -29,14 +107,14 @@ print "You said %r" % likes
 
 - prompt: A character that user can specify to ask for info from the user
 -------------------------------------------------------
-# ex15.py  Reading Files
+# ex15.py  "Reading Files"
 
 - open: Make a file available to Python
 - .: dot operator to join a function (or command or method) to an object
 - .read: Read the content of the file
 - .close: Close the file so it is not longer available to Python 
 --------------------------------------------------------
-# ex16.py Reading and Writ
+# ex16.py "Reading and Write"
 - w: write mode
 - r: read mode
 - a: append mode
@@ -48,7 +126,7 @@ print "You said %r" % likes
 - len(); command that returns the length of string
 - readline: Reads just one line of the text file. 
 --------------------------------------------------------
-# ex17.py More Files
+# ex17.py "More Files"
 - os.path: a library that adapts the file/directory address to the specific requiments of  the operating system
 - exists: a function to determine whether a specific files exists; yields a boolean True or False 
 
@@ -59,14 +137,18 @@ print "You said %r" % likes
 - def: to define custon functions
 - (*args): to refer to list of arguments in a function (but better to just list the arguments next to the function name)
 - : and intent: to set  block for function
+----------------------------------------------------------
+# ex19.py "Function and Variables"
+- More info on  using variables within functions but no new terms or symbols
+----------------------------------------------------------
+# ex20.py "Functions and Files"
+- .seek(): Go to specific position within a file (in bytes); .seak(0) - goes to the begining
+- readline: read one line from a file 
+- +=: shortcut for incrementing variable
+--------------
+# ex21.py "Functions Can Return Something"
 
-
-
-
-
-
-
-
+- return: a valuew that a function makes available to use in a variable.
 
 
 
