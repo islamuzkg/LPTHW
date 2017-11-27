b6131ab2ef980079349b056f802c4c427d431580
Islom Akhmedov <islamuzkg@gmail.com>
2017-11-27 10:56:33 -0600

    created exercise 28

---
+++ ex28/ex28.py
@@ -0,0 +1,79 @@
+print """1. \nMe: True and Falas = True?", "I would say False"
+Me: What about you?""" 
+print "Python replies: -------", True and False
+
+print """2. \nMe: False and True = True?", "I would say False"
+Me: What about you?"""
+print "Python replies: -------", False and True
+
+print """ 3. \n Me: 1 == 1 and 2 == 1 = True? I say it is False
+Me: What about you?"""
+print "Python replies: -------", 1 == 1 and 2 == 1
+
+print """ 4. \n Me: "test" == "test" = True? I say it is True
+Me: What about you?"""
+print "Python replies: -------", "test" == "test"
+
+print """ 5. \n Me: 1 == 1 or 2 != 1 = True? I say it is True
+Me: What about you?"""
+print "Python replies: -------", 1 == 1 and 2 != 1
+
+print """ 6. \n Me: True and 1 == 1 = True? I say it is True
+Me: What about you?"""
+print "Python replies: -------", True and 1 == 1
+
+print """ 7. \n Me: False and 0 != 0 = True? I say it is False
+Me: What about you?"""
+print "Python replies: -------", False and 0 != 0 
+
+print """ 8. \n Me: True or 1 == 1 = True? I say it is True
+Me: What about you?"""
+print "Python replies: -------", True or 1 == 1
+
+print """ 9. \n Me: "test" == "testing" = True? I say it is False
+Me: What about you?"""
+print "Python replies: -------", "test" == "testing"
+
+print """ 10. \n Me: 1 != 0 and 2 == 1 = True? I say it is False
+Me: What about you?"""
+print "Python replies: -------", 1 != 0 and 2 == 1
+
+print """ 11. \n Me: "test" != "testing" = True? I say it is True
+Me: What about you?"""
+print "Python replies: -------", "test" != "testing"
+
+print """ 12. \n Me: "test" == 1 = True? I say it is False
+Me: What about you?"""
+print "Python replies: -------",  "test" == 1
+
+print """ 13. \n Me: not (True and False) = True? I say it is True
+Me: What about you?"""
+print "Python replies: -------",  not (True and False)
+
+print """ 14.  not (1 == 1 and 0 != 1) = True? I say it is False
+Me: What about you?"""
+print "Python replies: -------", not (1 == 1 and 0 != 1)
+
+print """ 15. \n Me: not (10 == 1 or 1000 == 1000) = True? I say it is False
+Me: What about you?"""
+print "Python replies: -------",  not (10 == 1 or 1000 == 1000)
+
+print """ 16. \n Me: not (1 != 10 or 3 == 4) = True? I say it is False
+Me: What about you?"""
+print "Python replies: -------", not (1 != 10 or 3 == 4)
+
+print """ 17. \n Me: not ("testing" == "testing" and "Zed" == "Cool Guy")  = True? I say it is True
+Me: What about you?"""
+print "Python replies: -------", not ("testing" == "testing" and "Zed" == "Cool Guy")
+
+print """ 18. \n Me: 1 == 1 and (not ("testing" == 1 or 1 == 0)) = True? I say it is True
+Me: What about you?"""
+print "Python replies: -------",  1 == 1 and (not ("testing" == 1 or 1 == 0))
+
+print """ 19. \n Me: "chuncky" == "bacon" and (not (3 == 4 or 3 == 3)) = True? I say it is False
+Me: What about you?"""
+print "Python replies: -------", "chuncky" == "bacon" and (not (3 == 4 or 3 == 3))
+
+print """ 20. \n Me: 3 == 3 and (not ("testing" == "testing" or "Python" == "Fun")) = True? I say it is False
+Me: What about you?"""
+print "Python replies: -------",  3 == 3 and (not ("testing" == "testing" or "Python" == "Fun"))
