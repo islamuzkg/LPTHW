61a2c7b23b25d6fa3af706253cf7e2ec394ae965
Islom Akhmedov <islamuzkg@gmail.com>
2017-12-19 21:24:10 -0600

    Renaming

---
+++ ex34_AccessingElementsOfLists/ex34.py
@@ -0,0 +1,17 @@
+
+animals = ['bear', 'tiget', 'penguin', 'zebrq']
+bear = animals[0]
+print bear
+
+# most of the list related exercises I have done in ex32.
+
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print '1. The animal at 1 is ', animals[1]
+print "2. The third one is %s" % animals[2]
+print "3. The 1st animal is", animals[0]
+print "4. The animal at 3 is ", animals[3]
+print "5. The fifth animal is", animals[4]
+print "6. The animal at 2 is", animals[1]
+print "7. The sixth animal is", animals[5]
+print "8. The animal at 4 is", animals[4]
