cca61e17e219f5d78c0832cf80ba013c9b9ec46e
Islom Akhmedov <islamuzkg@gmail.com>
2018-01-01 14:30:55 -0600

    updating 37

--- ex37_SymbolReview/ListOfKeywordsInPython.vb
+++ ex37_SymbolReview/ListOfKeywordsInPython.vb
@@ -107,6 +107,120 @@ None
 
 Although this function has a return statement, it is not reached in every case. The function will ruturn True only when the input is even.
 
+If we give the function an odd number, "Nore" is returned implicitly.
+
+
+and, or, not
+
+and, or, not are the logical operators in Python. and will result into True only if both the operands are True. The truth table for and is given below:
+
++-----------------+---------------+
+|AND              |True?          |
++-----------------+---------------+
+|True and False   |False          |
++-----------------+---------------+
+|True and True    |True           |
++-----------------+---------------+
+|False and True   |False          |
++-----------------+---------------+
+|False and False  |False          |
++-----------------+---------------+
+
+or will result True if any of the operands is True. The truth table for or is given below: 
+
++----------------+---------------+
+|OR              |True?          |
++----------------+---------------+
+|True or False   |True           |
++----------------+---------------+
+|True or True    |True           |
++----------------+---------------+
+|False or True   |True           |
++----------------+---------------+
+|False or False  |False          |
++----------------+---------------+
+
+not operator is used to invert the truth value. The truth table for not is given below:
+
++---------------+---------------+
+|NOT		|True?		|
++-------------------------------+
+|not False	|True		|
++---------------+---------------+
+|not True	|False		|
++---------------+---------------+
+
+
+as
+
+as is used to create an alias while importing a  module. It means giving a different name (user-defined) to a module while importing it.
+
+As for example, Python has a standard module called math. Suppose we want to calculate what consine pi id using an alias. We can do it as follows using as:
+
+-------------------------------
+>>> import math as myAlias
+>>> myAlias.cos(myAlias.pi)
+-1.0
+>>>
+------------------------------
+
+Here we imported the "math" module by giving the name myAlias. We can refer to the "math" module with this name. Using this name we calculated cos(pi) and we got -1.0 as the answer.
+
+
+assert is used for debugging purposes.
+
+While programming, sometime we wish to know the internal state or check if our assumptions are true. assert helps us to do this and find bugs more conveniently. assert is followrf by a condition.
+
+If the condiiton is true, nothing happens. But if the condition is false, AssertionError is raised.
+For example: 
+------------------------------
+>>> a = 4
+>>> assert a < 5
+>>> assert a > 5, "The value of a is too small"
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+AssertionError: The value of a is too small
+----------------------------
+
+For our better understanding, we can also provide a message to be printed with the AsserionError.
+
+
+At this point we can note that,
+
+assert condition, message
+
+is equivalent to,
+
+if not condition:
+    raise AssertionError(message)
+
+break, continue
+
+break and continue are used inside for and while loops ro alter theor normal behavior.
+
+break will end the smalest loop in is in and control flows to statement immediately below the loop. continue casuses tothe end the current  iteration of the loop, but not the whole loop.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
