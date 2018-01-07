4880f6b7f4261dc43813fc7f230eeaa77fec8e1e
Islom Akhmedov <Islamuzkg@gmail.com>
2018-01-07 09:06:58 -0600

    update

--- ex37_SymbolReview/ListOfKeywordsInPython.vb
+++ ex37_SymbolReview/ListOfKeywordsInPython.vb
@@ -777,3 +777,77 @@ lists 		Stores a list of things. 		j = [1,2,3,4]
 -----------------------------------------------------------------------------
 dicts 		Stores a key=value mapping of things. 	e = {’x’: 1, ’y’: 2}		
 -----------------------------------------------------------------------------
+
+
+String Escape Sequences
+
+Escape Description
+\\ Backslash
+\’ Single-quote
+\” Double-quote
+\a Bell
+\b Backspace
+\f Formfeed
+\n Newline
+\r Carriage
+\t Tab
+\v Vertical tab
+
+String Formats
+Same thing for string formats: use them in some strings to know what they do.
+
+Escape Description Example
+%d Decimal integers (not floating point). ”%d” % 45 == ’45’
+%i Same as %d. ”%i” % 45 == ’45’
+%o Octal number. ”%o” % 1000 == ’1750’
+%u Unsigned decimal. ”%u” % -1000 == ’-1000’
+%x Hexadecimal lowercase. ”%x” % 1000 == ’3e8’
+%X Hexadecimal uppercase. ”%X” % 1000 == ’3E8’
+%e Exponential notation, lowercase ’e’. ”%e” % 1000 == ’1.000000e+03’
+%E Exponential notation, uppercase ’E’. ”%E” % 1000 == ’1.000000E+03’
+%f Floating point real number. ”%f” % 10.34 == ’10.340000’
+%F Same as %f. ”%F” % 10.34 == ’10.340000’
+%g Either %f or %e, whichever is shorter. ”%g” % 10.34 == ’10.34’
+%G Same as %g but uppercase. ”%G” % 10.34 == ’10.34’
+%c Character format. ”%c” % 34 == ’”’
+%r Repr format (debugging format). ”%r” % int == ”<type ’int’>”
+%s String format. ”%s there” % ’hi’ == ’hi
+there’
+%% A percent sign.	”%g%%” % 10.34 == ’10.34%’
+
+Operators
+
+Some of these may be unfamiliar to you, but look them up anyway. Find out what they do, and if you
+still can’t figure it out, save it for later.
+
+Operator Description Example
++ Addition 2 + 4 == 6
+- Subtraction 2 - 4 == -2
+* Multiplication 2 * 4 == 8
+** Power of 2 ** 4 == 16
+/ Division 2 / 4.0 == 0.5
+// Floor division 2 // 4.0 == 0.0
+% String interpolate or modulus 2 % 4 == 2
+< Less than 4 < 4 == False
+> Greater than 4 > 4 == False
+<= Less than equal 4 <= 4 == True
+>= Greater than equal 4 >= 4 == True
+== Equal 4 == 5 == False
+!= Not equal 4 != 5 == True
+<> Not equal 4 <> 5 == True
+( ) Parenthesis len(’hi’) == 2
+[ ] List brackets [1,3,4]
+{ } Dict curly braces {’x’: 5, ’y’: 10}
+@ At (decorators) @classmethod
+, Comma range(0, 10)
+: Colon def X():
+. Dot self.x = 10
+= Assign equal x = 10
+; semi-colon print ”hi”; print ”there”
++= Add and assign x = 1; x += 2
+-= Subtract and assign x = 1; x -= 2
+*= Multiply and assign x = 1; x *= 2
+/= Divide and assign x = 1; x /= 2
+//= Floor divide and assign x = 1; x //= 2
+%= Modulus assign x = 1; x %= 2
+**= Power assign x = 1; x **= 2
