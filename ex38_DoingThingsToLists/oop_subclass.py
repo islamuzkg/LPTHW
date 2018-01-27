73b323f56042634553dbc820e73215a2e27ab62a
Islom Akhmedov <islamuzkg@gmail.com>
2018-01-27 08:22:28 -0600

    updating

--- ex38_DoingThingsToLists/oop_subclass.py
+++ ex38_DoingThingsToLists/oop_subclass.py
@@ -8,9 +8,7 @@ class SchoolMember:
 
 	def tell(self):
 		'''Tell my details.'''
-		print('Name:"{}" Age:"{}"'.format(self.name, self.age), end='')
-		#print('Name:"{}" Age:"{}"'.format(self.name, self.age), x)
-
+		print('Name:"{}" Age:"{}"'.format(self.name, self.age), end=" ")
 
 
 class Teacher(SchoolMember):
@@ -23,18 +21,18 @@ class Teacher(SchoolMember):
 	def tell(self):
 		SchoolMemeber.tell(self)
 		print('Salary: "{:d}"'.fomat(self.salary)
-"""
-class Student(SchoolMember):
-	'''Represents a student.'''
-	def __init__(self, name, age, marks):
-		SchoolMember.__init__(self, name, age)
-		self.marks = marks
-		print('(Initialized Student: {})'.format(self.name))
 
-	def tell(self):
-		SchollMember.tell(self):	
-		print('Marks: "{:d}"'.format(self.marks))
-"""
+
+#class Student(SchoolMember):
+#	'''Represents a student.'''
+#	def __init__(self, name, age, marks):
+#		SchoolMember.__init__(self, name, age)
+#		self.marks = marks
+#		print('(Initialized Student: {})'.format(self.name))
+#
+#	def tell(self):
+#		SchollMember.tell(self):	
+#		print('Marks: "{:d}"'.format(self.marks))
 
 t = Teacher('Mrs. Shrivdiya', 40, 30000)
 # s = Student('Swaroop', 25, 75)
