9d740ac5e980eba0166e50b7d9d60cec0d98a359
Islom Akhmedov <islamuzkg@gmail.com>
2018-02-27 11:08:58 -0600

    Some changes

--- ex42_Is-A_Has-A_Object_and_Classes/staticMethodPizza.py
+++ ex42_Is-A_Has-A_Object_and_Classes/staticMethodPizza.py
@@ -1,6 +1,20 @@
 # https://dbader.org/blog/python-string-formatting
-#  https://en.wikipedia.org/wiki/Don't_repeat_yourself
-# http://effbot.org/pyfaq/tutor-whats-the-difference-between-import-foo-and-from-foo-import.htm
-
-
+#https://realpython.com/blog/python/instance-class-and-static-methods-demystified/
 import math
+
+class Pizza:
+    def __init__(self, radius, ingredients):
+        self.radius = radius
+        self.ingredients = ingredients
+
+    def __repr__(self):
+        return (f'Pizza({self.radius!r}, '
+                f'{self.ingredients!r})')
+
+
+    def area(self):
+        return self.circle_area(self.radius)
+
+    @staticmethod
+    def circl_area(r):
+        return r ** 2 * math.pi
