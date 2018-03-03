2c0ca6cd1a7a96a15ff20c8714d4fe635c67351e
Islom Akhmedov <islamuzkg@gmail.com>
2018-03-03 08:02:50 -0600

    updating

--- ex42_Is-A_Has-A_Object_and_Classes/staticMethodPizza.py
+++ ex42_Is-A_Has-A_Object_and_Classes/staticMethodPizza.py
@@ -1,6 +1,7 @@
 # https://dbader.org/blog/python-string-formatting
-#  https://en.wikipedia.org/wiki/Don't_repeat_yourself
 # http://effbot.org/pyfaq/tutor-whats-the-difference-between-import-foo-and-from-foo-import.htm
 
-
 import math
+
+class Pizza:
+	def __init__(self, radius, ingredients):
