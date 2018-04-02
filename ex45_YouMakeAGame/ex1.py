8572c9e6884c4a1da3d37041374d426a420e7064
Islom Akhmedov <islamuzkg@gmail.com>
2018-04-02 11:33:30 -0500

    Some changes

--- ex45_YouMakeAGame/ex1.py
+++ ex45_YouMakeAGame/ex1.py
@@ -1,1 +1,11 @@
 # Work is in progress
+Traceback (most recent call last):
+  File "ex43.py", line 238, in <module>
+    a_game.play()
+  File "ex43.py", line 20, in play
+    current_scene = self.scene_map.opening_scene()
+  File "ex43.py", line 234, in opening_scene
+    return self.next_scene(self.start_scene)
+  File "ex43.py", line 230, in next_scene
+    val = Map.scenes.get(scene.name)
+NameError: global name 'scene' is not defined
