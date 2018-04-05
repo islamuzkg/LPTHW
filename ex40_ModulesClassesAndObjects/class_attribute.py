d7cf11f67a2e04389de14c015c1e18b5a4afe87d
Islom Akhmedov <islamuzkg@gmail.com>
2018-04-05 12:28:05 -0500

    Some changes

--- ex40_ModulesClassesAndObjects/class_attribute.py
+++ ex40_ModulesClassesAndObjects/class_attribute.py
@@ -1,5 +1,5 @@
 # https://stackoverflow.com/questions/47137880/difference-between-inheritance-and-composition-in-a-mvc-architecture
-
+# https://www.cs.uct.ac.za/mit_notes/python/Classes.html
 class Person:
 
     TITLES = ('Dr', 'Mr', 'Mrs', 'Ms')
@@ -24,7 +24,7 @@ class Person1:
         return self.deceased
 
 person1 = Person1()
-print ">>> This output is from Person1"
+print ">>>>>>>>> This output is from Person1"
 print person1.mark_as_deceased()
 
 
@@ -40,7 +40,7 @@ jane = Person2()
 bob = Person2()
 
 jane.add_pet("cat")
-print ">>> This output is from Person2"
+print ">>>>>>>> This output is from Person2"
 print jane.pets
 print bob.pets
 
@@ -55,7 +55,89 @@ class Smith:
 
 person = Smith('Alen')
 
-print ">>> This output is from Smith"
+print ">>>>>>>>> This output is from Smith"
 print person.name # name is instance attribute
 print person.surname # surname is class attribute
 print person.profession
+
+
+
+
+class Person3:
+    TITLES = ('Dr', 'Mr', 'Mrs', 'Ms')
+
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+
+    def fullname(self): #instance method
+         # instance object accessible through self
+         return "%s %s" % (self.name, self.surname)
+
+    @classmethod
+    def allowed_titles_starting_with(cls, startswith): #class method
+        # class or instance object acccessible through cls
+        return [t for t in cls.TITLES if t.startswith(startswith)]
+
+    @staticmethod
+    def allowed_titles_ending_with(endswith):   #static method
+        # no parameter for class or instance object
+        # we have to use Person3 directly
+        return [t for t in Person3.TITLES if t.endswith(endswith)]
+
+jane = Person3("Jane", "Smith")
+
+print ">>>>>>>>>>>>>>> Below output is from Person3"
+print jane.fullname()
+
+print ">>>>>>> Person3 classic method"
+print jane.allowed_titles_starting_with("M")
+print Person3.allowed_titles_starting_with("M")
+
+print ">>>>>>> Person3 static method"
+print jane.allowed_titles_ending_with("s")
+print Person3.allowed_titles_ending_with("s")
+
+
+class Numbers:
+    MULTIPLIER = 8.9
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def add(self):
+        return self.x + self.y
+
+    @classmethod
+    def multiply(cls, a):
+        return cls.MULTIPLIER * a
+
+    @staticmethod
+    def substract(b, c):
+        return b -c
+
+    @property
+    def value(self):
+        return (self.x - self.y)
+
+    @value.setter
+    def value(self, xy_tuple):
+        self.x, self.y = xy_tuple
+
+    @value.deleter
+    def value(self):
+        del self.x
+        del self.y
+
+
+
+
+num = Numbers(4, 5)
+
+print ">>>>>>>>>> Below is output from Math"
+print num.add()
+print num.multiply(5)
+print num.substract(5, 3)
+print ">>>>>>>>>>>>>>>>"
+print num.value
